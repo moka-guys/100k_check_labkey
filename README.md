f333ed0b5fe47e56fb32c917411d72190bbf2c06
Andy Bond <andybond87@gmail.com>
2019-02-28 09:51:07 +0000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# 100k_check_labkey
+# jellypy_labkey v1.0
+
+## About
+Get data from the Genomics England LabKey API for a given participant ID. The API is accessed using the HTTP interface.
+
+The class LabKey_HTTP() contains methods for accessing data. Calling the script prints the following to the console for the input PID:
+"<NAME>,<DATE_OF_BIRTH\[DAY/MONTH/YEAR\]>,<NHS NUMBER>"
+
+This module requires a ~/.netrc file with a user's LabKey login credentials. Details of this file format can be found in the [LabKey API Docs on creating a netrc file](https://www.labkey.org/Documentation/wiki-page.view?name=netrc).
+
+## Quickstart
+```
+LabKey.py --pid 1234567 -u [ labkey username ] -p [ labkey password ]
+
+Arguments:
+    pid: A GEL participant ID
+    username;password: Optional labkey credentials
+```
+
+## Objects
+Class LabKey_HTTP(pid, username=None, password=None) : Returns an object with LabKey data for a given GEL participant ID.
---
+++ test_labkey.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import pytest
+from . import LabKey
+
+# Read test patient data
+with open('test_data.txt') as f:
+    PID, DOB, NHSID = [ line.rstrip('\n') for line in f.readlines() ]
+# Read authentication data
+with open('/home/mokaguys/.netrc') as f:
+    csv = [ line.split(' ') for line in f.readlines() ]
+    MACHINE, UNAME, PWD = [ columns[1].rstrip('\n') for columns in csv ]
+
+def test_optional_inputs():
+    import sys
+    input_set1 = ['python', '-i', str(PID)]
+    input_set2 = ['python', '-i', PID, '-u', UNAME, '-p', PWD ]
+    # Test with no user details
+    sys.argv = input_set1
+    LabKey.main()
+    # Test with user details
+    sys.argv = input_set2
+    LabKey.main()
+
+@pytest.fixture
+def lk_object():
+    '''Return Labkey object for test data PID'''
+    return LabKey.LabKey_HTTP(PID, UNAME, PWD)
+
+def test_labkey_dict(lk_object):
+    assert isinstance(lk_object.data, dict)
+
+def test_date_of_birth(lk_object):
+    assert lk_object.dob.startswith(DOB)
+
+def test_nhs_number(lk_object):
+    assert lk_object.nhsid == NHSID
